e6ad300227c19f90aacf7802ffc9bf086bb1ef19
Tiziano26 <132932851+Tiziano26@users.noreply.github.com>
2024-05-14 21:15:44 -0300

    hello

---
+++ src/javaEjemplo1/Persona.java
@@ -0,0 +1,40 @@
+package javaEjemplo1;
+
+public class Persona {
+private String nombre;
+private String apellido;
+private int edad;
+
+public Persona(String nombre, String apellido) {
+	this.nombre = nombre;
+	this.apellido = apellido;
+}
+
+public String saludar() {
+	return "Hola soy "+ nombre + " y mi apellido es "+ apellido+ " tengo "+ edad + "años";
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getApellido() {
+	return apellido;
+}
+
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+
+public int getEdad() {
+	return edad;
+}
+
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+}
